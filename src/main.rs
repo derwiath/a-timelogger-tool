4dd1b8a38371400c36ce3c7689e37b3e637b310b
Andreas Andersson <andreas@herrandersson.se>
2020-02-26 19:32:13 +0100

    Print month summary

--- src/main.rs
+++ src/main.rs
@@ -69,20 +69,27 @@ fn main() {
     let year: u32 = 2020;
 
     let mut date_seconds = HashMap::new();
+    let mut month_seconds = HashMap::new();
     for entry in entries {
         let from_dt = parse_datetime(year, entry.from).unwrap();
         let to_dt = parse_datetime(year, entry.to).unwrap();
         assert_eq!(from_dt.date(), to_dt.date());
         let date = from_dt.date();
         let duration = to_dt.signed_duration_since(from_dt);
-        let seconds = date_seconds.entry(date).or_insert(0);
+        {
+            let seconds = date_seconds.entry(date).or_insert(0);
+            *seconds += duration.num_seconds();
+            println!(
+                "{} +{:02}:{:02}",
+                date,
+                duration.num_hours(),
+                duration.num_minutes() % 60
+            );
+        }
+
+        let month = chrono::NaiveDate::from_ymd(date.year(), date.month(), 1);
+        let seconds = month_seconds.entry(month).or_insert(0);
         *seconds += duration.num_seconds();
-        println!(
-            "{} +{:02}:{:02}",
-            date,
-            duration.num_hours(),
-            duration.num_minutes() % 60
-        );
     }
 
     for date in date_seconds.keys().sorted() {
@@ -93,6 +100,17 @@ fn main() {
             date,
             date.iso_week().week(),
             date.weekday(),
+            duration.num_hours(),
+            duration.num_minutes() % 60
+        );
+    }
+    for date in month_seconds.keys().sorted() {
+        let seconds = month_seconds.get(date).unwrap();
+        let duration = chrono::Duration::seconds(*seconds);
+        println!(
+            "{}-{:02}-** {:03}:{:02}",
+            date.year(),
+            date.month(),
             duration.num_hours(),
             duration.num_minutes() % 60
         );
