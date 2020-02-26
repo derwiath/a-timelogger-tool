1b9c4f177cec0b2b924f9947ad56132e8e43cadf
Andreas Andersson <andreas@herrandersson.se>
2020-02-26 08:43:46 +0100

    Calculate duration

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use chrono::NaiveDateTime;
 use std::env;
 use std::fs;
+
+extern crate chrono;
 
 #[derive(Debug)]
 struct ReportEntry<'a> {
@@ -45,6 +48,14 @@ fn read_report<'a>(file_contents: &'a String) -> Vec<ReportEntry<'a>> {
     entries
 }
 
+fn parse_datetime(
+    year: u32,
+    entry_datetime: &str,
+) -> Result<chrono::NaiveDateTime, chrono::ParseError> {
+    let s = format!("{} {}", year, entry_datetime);
+    NaiveDateTime::parse_from_str(s.as_str(), "%Y %d %b %H:%M")
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let name = args.get(0).expect("Failed to get executable name");
@@ -53,8 +64,19 @@ fn main() {
         .expect(format!("Usage: {} <report-csv>", name).as_str());
     let file_contents = fs::read_to_string(filename).expect("Failed to read report file");
     let entries = read_report(&file_contents);
+    let year: u32 = 2020;
 
     for entry in entries {
         println!("{:?}", entry);
+        let from_dt = parse_datetime(year, entry.from).unwrap();
+        let to_dt = parse_datetime(year, entry.to).unwrap();
+        assert_eq!(from_dt.date(), to_dt.date());
+        let date = from_dt.date();
+        let duration = to_dt.signed_duration_since(from_dt);
+        println!(
+            "  duration {:02}:{:02}",
+            duration.num_hours(),
+            duration.num_minutes() % 60
+        );
     }
 }
