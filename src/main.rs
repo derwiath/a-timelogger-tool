c1cac80c3891c51b1f13863a1dd643a9b4ef516e
Andreas Andersson <andreas@herrandersson.se>
2020-02-26 07:04:00 +0100

    Parse entries from report

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+use std::env;
+use std::fs;
+
+#[derive(Debug)]
+struct ReportEntry<'a> {
+    activity: &'a str,
+    duration: &'a str,
+    from: &'a str,
+    to: &'a str,
+    comment: &'a str,
+}
+
+impl<'a> ReportEntry<'_> {
+    pub fn new(
+        activity: &'a str,
+        duration: &'a str,
+        from: &'a str,
+        to: &'a str,
+        comment: &'a str,
+    ) -> ReportEntry<'a> {
+        ReportEntry {
+            activity,
+            duration,
+            from,
+            to,
+            comment,
+        }
+    }
+}
+
+fn read_report<'a>(file_contents: &'a String) -> Vec<ReportEntry<'a>> {
+    let mut entries = Vec::<ReportEntry<'a>>::new();
+    for line in file_contents.lines() {
+        let tokens: Vec<&str> = line.split(';').collect();
+        if tokens.len() != 5 {
+            break;
+        }
+        if tokens[0] == "Activity type" {
+            continue;
+        }
+        entries.push(ReportEntry::new(
+            tokens[0], tokens[1], tokens[2], tokens[3], tokens[4],
+        ));
+    }
+    entries
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let name = args.get(0).expect("Failed to get executable name");
+    let filename = args
+        .get(1)
+        .expect(format!("Usage: {} <report-csv>", name).as_str());
+    let file_contents = fs::read_to_string(filename).expect("Failed to read report file");
+    let entries = read_report(&file_contents);
+
+    for entry in entries {
+        println!("{:?}", entry);
+    }
 }
